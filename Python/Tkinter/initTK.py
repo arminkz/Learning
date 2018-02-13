49bc5a84fb17ddfc396b7f8113589d5140f1ff3d
Armin Kazemi <arminkz@live.com>
2018-02-13 22:09:58 +0330

    PhotoImage Label

--- Python/Tkinter/initTK.py
+++ Python/Tkinter/initTK.py
@@ -1,8 +1,18 @@
-from Tkinter import *
+import Tkinter as tk
 
-root = Tk()
+root = tk.Tk()
 
-w = Label(root,text="Hello World !")
-w.pack()
+logo = tk.PhotoImage(file="snake_image.gif")
+logolabel = tk.Label(root,image=logo).pack(side="right")
+
+explaination = """At present, only GIF and PPM/PGM
+formats are supported, but an interface 
+exists to allow additional image file
+formats to be added easily."""
+
+explanationlabel = tk.Label(root,
+                            justify=tk.LEFT,
+                            padx = 10,
+                            text = explaination).pack(side="left")
 
 root.mainloop()
